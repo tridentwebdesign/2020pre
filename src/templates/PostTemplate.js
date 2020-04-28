8c8f14118647d6f90d109817e365eb5e43e93633
河口 英生 <kawaguchi@tc.kawai-juku.ac.jp>
2020-04-28 16:29:10 +0900

    metaOK

--- src/templates/PostTemplate.js
+++ src/templates/PostTemplate.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout/layout"
+import { Helmet } from "react-helmet"
 
 
 export const pageQuery = graphql`
@@ -20,23 +21,37 @@ export default function (props) {
   const post = props.data.markdownRemark
   const { frontmatter } = post
   return (
-    <Layout>
-      {
-        <main className="is-full container">
-          <section className="section">
-            <h2 className="title is-4">{frontmatter.title}</h2>
-            <div className="content sentence">
-              <div dangerouslySetInnerHTML={{ __html: post.html }} />
-            </div>
-          </section>
-          <section className="section report">
-            <ul>
-              <li>投稿日: {frontmatter.date}</li>
-              <li>投稿者: {frontmatter.author}</li>
-            </ul>
-          </section>
-        </main>
-      }
-    </Layout>
+    <div>
+      <Helmet
+        htmlAttributes={{
+          lang: 'ja',
+        }}
+        title={frontmatter.title}
+        meta={[
+          {
+            name: 'description',
+            content: props.description || 'Gastbyサンプル'
+          }
+        ]}
+      />
+      <Layout>
+        {
+          <main className="is-full container">
+            <section className="section">
+              <h2 className="title is-4">{frontmatter.title}</h2>
+              <div className="content sentence">
+                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+              </div>
+            </section>
+            <section className="section report">
+              <ul>
+                <li>投稿日: {frontmatter.date}</li>
+                <li>投稿者: {frontmatter.author}</li>
+              </ul>
+            </section>
+          </main>
+        }
+      </Layout>
+    </div>
   )
 }
