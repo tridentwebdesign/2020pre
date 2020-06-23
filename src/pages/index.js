57a5817fcaeacbe5b0372401479d01f8a00fcb7c
河口英生 <hideo_k@red.zero.jp>
2020-06-23 22:36:21 +0900

    業界研究画像シリーズ

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,6 +51,7 @@ const IndexPage = () => {
                                                 <li><Link to="/industry/post-2">Webサーバー</Link></li>
                                                 <li><Link to="/industry/post-3">ネットワーク技術</Link></li>
                                                 <li><Link to="/industry/post-4">HTTP</Link></li>
+                                                <li><Link to="/industry/post-5">Webのさまざまなデータ形式</Link></li>
                                             </ol>
                                         </div>
                                     </div>
