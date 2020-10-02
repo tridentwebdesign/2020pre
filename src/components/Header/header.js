71b237714c4e97465e8a141fc8f1d43f120e3602
tridentwebdesign <webdesigntrident@gmail.com>
2020-10-03 08:01:36 +0900

    2020前期をboldに変更

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,7 +6,7 @@ export default () => (
         <div className="hero-body">
             <div className="container">
                 <h1 className="title is-2 is-spaced"><Link to="/">Webデザイン学科 授業資料</Link></h1>
-                <h2 className="subtitle"><span className="period"><Link to="/">2020前期</Link></span><span className="period"><Link to="https://2020late.hideok.org/">2020後期</Link></span></h2>
+                <h2 className="subtitle"><span className="period has-text-weight-bold"><Link to="/">2020前期</Link></span><span className="period"><Link to="https://2020late.hideok.org/">2020後期</Link></span></h2>
             </div>
         </div>
     </header>
